6c599e75c390a2594c79cf6fb95bd8a069e12c86
Hansini Ruparel <hansini.ruparel@gmail.com>
2021-06-06 18:02:48 +0530

    Add files via upload
    In this, I am trying to understand concept of variables, constants and reserved words in Python

---
+++ concept-variables1.py
@@ -0,0 +1,15 @@
+#Variables: a named place in memory. Used to store data and retrieve it later using variable name
+#Constants: fixed values such as numbers, strings etc. Value DOES NOT change
+#Reserved Words: fixed keywords in a language like: if, else etc
+
+#Example of a constant and a variable
+
+a = 3.5
+b = 4
+c = float(input("Enter a number "))
+
+print("Value of a ",a)
+print("Value of b ",b+1)
+print("Value of c ",c+1)
+
+#in this case a - constant and b and c are variables. Reserved words are: print, float, input
