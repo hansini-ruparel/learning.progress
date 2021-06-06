7080b4263d0d0cf63b579961bbaf5b31d85371ef
Hansini Ruparel <hansini.ruparel@gmail.com>
2021-06-06 23:07:50 +0530

    Add files via upload

---
+++ concept-variables2.py
@@ -0,0 +1,31 @@
+#Various Data Types:
+#int - integer data type, only whole nos with no decimal
+#float - a number with decimals
+#str - string or combining all character like abc, blah
+
+a = 2 #integer variable
+b = 2.5 #float variable
+c = 'two' #string variable
+
+print (a, type(a))
+print (b, type (b))
+print (c, type(c))
+
+#Type Conversion: When you put an integer and floating point like implicitly converting it
+
+
+d = 2.0
+int (d) #float --> integer
+print (d, type(a))
+
+e = 9
+e = float (e) #integer --> float
+print (e, type(e))
+
+f = 17
+f = str(f) #integer --> string
+print (f, type(f))
+
+g = 24.6
+g = str(g) #float --> sting
+print (g, type(g))
